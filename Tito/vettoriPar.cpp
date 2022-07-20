a58cdeba3dad2bdd3679c309677f59bda9868fcc
Elia Menoni <menonielia00@gmail.com>
2022-07-20 08:24:26 +0000

    Teoria Vettori - Vettoria paralleli

---
+++ Tito/vettoriPar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    char inizialeNome[100];
+    int eta[100];
+    float altezza[100];
+
+    // Elemento 5
+    // Tito
+    inizialeNome[4] = 'T';
+    eta[4] = 17;
+    altezza[4] = 1.9;
+
+    // Elia
+    inizialeNome[5] = 'E';
+    eta[5] = 22;
+    altezza[5] = 1.85;
+}
