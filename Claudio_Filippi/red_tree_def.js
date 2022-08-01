b13a9960301f552ac773ac8f71145667ec519eda
Elia Menoni <menonielia00@gmail.com>
2022-08-01 21:57:15 +0000

    Esercizio ricorsione - Definizione Albero

---
+++ Claudio_Filippi/red_tree_def.js
@@ -0,0 +1,14 @@
+function tree_def(depth, figli, root = []) {
+    if (depth == 1) return root["Val"] = Math.floor(Math.random() * 10);
+
+    root["Val"] = Math.floor(Math.random() * 10);
+
+    for (let i = 0; i < figli; i++) {
+        let node = tree_def(depth - 1, figli, []);
+        if (node != null) root["Leaf" + (i + 1)] = node;
+    }
+    return root
+}
+
+let tree = tree_def(5,2);
+console.log(tree);
