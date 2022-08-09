f787544bac4edeb6ebdcfff24b4fb6edc7a478fb
Elia Menoni <menonielia00@gmail.com>
2022-08-09 08:28:30 +0000

    Fine esercizio ricorsione

--- Tito_Laficara/rec_pal.cpp
+++ Tito_Laficara/rec_pal.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int conta (char str[]){
-int conteggio = 0;
-    while (str[conteggio]){
-        conteggio ++;
-    }
-    return conteggio;
+int conta(char str[]) {
+  int conteggio = 0;
+  while (str[conteggio]) {
+    conteggio++;
+  }
+  return conteggio;
 }
 
-bool palindroma (char str[]){
-    if (str[0] == '*' && conta (str) == 1){
-        return true;
-    }
+void trasferimento(char str[], char str2[], int indice1, int indice2) {
+  int j = 0;
+  for (int i = indice1; i <= indice2; i++) {
+    str2[j] = str[i];
+    j++;
+  }
+  str2[j] = '\0';
+}
 
+bool palindroma(char str[]) {
+  if (str[0] == '*' && conta(str) == 1) {
+    return true;
+  }
+  if (str[0] == str[conta(str) - 1]) {
+    char str2[conta(str) - 1];
+    trasferimento(str, str2, 1, conta(str) - 2);
+    return palindroma(str2);
+  }
+  return false;
 }
 
 int main(void) {
+  char str[] = "abc*cba";
+  cout << palindroma(str) << endl;
+  char str2[] = "abc*bba";
+  cout << palindroma(str2) << endl;
+  char str3[] = "abcd*cba";
+  cout << palindroma(str3) << endl;
 
   return 0;
 }
