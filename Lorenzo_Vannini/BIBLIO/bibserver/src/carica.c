bf73829d61a8997401076a54975ec8543c015e5b
Lore398 <138612328+Lore398@users.noreply.github.com>
2023-07-18 13:55:36 +0000

    aggiornamento

--- Lorenzo_Vannini/BIBLIO/bibserver/src/carica.c
+++ Lorenzo_Vannini/BIBLIO/bibserver/src/carica.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include "unboundedqueue.h"
 
 struct Libro {
   int numero_autori;
@@ -23,6 +25,8 @@ struct Nodo {
 };
 
 typedef struct Nodo Nodo;
+
+pthread_mutex_t buffer_mutex;
 
 void stampa(Libro *libro) {
   for (int j = 0; j < libro->numero_autori; j++)
@@ -103,20 +107,36 @@ Nodo* riempi_catalogo(char *file) {
   return catalogo;
 }
 
+void copia(Libro* destinazione, Libro* sorgente){
+  destinazione -> numero_autori = sorgente -> numero_autori;
+  for(int i = 0; i<sorgente->numero_autori; i++){
+    strcpy(destinazione -> autore[i], sorgente -> autore[i]);
+  }
+  strcpy(destinazione -> titolo, sorgente -> titolo);
+  strcpy(destinazione -> editore, sorgente -> editore);
+  destinazione -> anno = sorgente -> anno;
+  strcpy(destinazione -> nota, sorgente -> nota);
+  strcpy(destinazione -> collocazione, sorgente -> collocazione);
+  strcpy(destinazione -> luogo_pubblicazione, sorgente -> luogo_pubblicazione);
+  strcpy(destinazione -> descrizione_fisica, sorgente -> descrizione_fisica);
+  if(sorgente -> prestito != NULL)
+    strcpy(destinazione -> prestito, sorgente -> prestito);
+  else
+    destinazione -> prestito = NULL;
+}
 
-
-Nodo* ricerca_libri(Nodo* testa_lista, Libro* filtri){
+Nodo* ricerca_libri(Nodo* cursore, Libro* filtri){
    int valido;
    Nodo* invia = NULL;
    Nodo* nodo_invia = NULL;
    
-   while (testa_lista){
+   while (cursore){
     valido = 1;
     
     if(strcmp(filtri -> autore[0], "") != 0){
       int trovato = 0;
-      for(int i = 0; i < testa_lista -> libro -> numero_autori; i++)
-        if(strstr(testa_lista -> libro -> autore[i], filtri -> autore[0]))
+      for(int i = 0; i < cursore -> libro -> numero_autori; i++)
+        if(strstr(cursore -> libro -> autore[i], filtri -> autore[0]))
           trovato++;
       if(trovato > 0)
         valido = valido & 1;
@@ -125,42 +145,42 @@ Nodo* ricerca_libri(Nodo* testa_lista, Libro* filtri){
     }
     
     if(strcmp(filtri -> titolo, "") != 0){  //guarda se esistono sottostringhe della seconda parola all'interno della prima
-      if(strstr(testa_lista -> libro -> titolo, filtri -> titolo))
+      if(strstr(cursore -> libro -> titolo, filtri -> titolo))
         valido = valido & 1;
       else
         valido = valido & 0;
     }
     
     if(strcmp(filtri -> editore, "") != 0){  //guarda se esistono sottostringhe della seconda parola all'interno della prima
-      if(strstr(testa_lista -> libro -> editore, filtri -> editore))
+      if(strstr(cursore -> libro -> editore, filtri -> editore))
         valido = valido & 1;
       else
         valido = valido & 0;
     }
 
     if(filtri -> anno != -1){
-      if(filtri -> anno == testa_lista -> libro -> anno)
+      if(filtri -> anno == cursore -> libro -> anno)
         valido = valido & 1;
       else
         valido = valido & 0;
     }
 
     if(strcmp(filtri -> collocazione, "") != 0){  
-      if(strstr(testa_lista -> libro -> collocazione, filtri -> collocazione))
+      if(strstr(cursore -> libro -> collocazione, filtri -> collocazione))
         valido = valido & 1;
       else
         valido = valido & 0;
     }
     
     if(strcmp(filtri -> descrizione_fisica, "") != 0){  
-      if(strstr(testa_lista -> libro -> descrizione_fisica, filtri -> descrizione_fisica))
+      if(strstr(cursore -> libro -> descrizione_fisica, filtri -> descrizione_fisica))
         valido = valido & 1;
       else
         valido = valido & 0;
     }
 
     if(filtri -> prestito != NULL){
-      if(strstr(testa_lista -> libro -> prestito, filtri -> prestito))
+      if(strstr(cursore -> libro -> prestito, filtri -> prestito))
         valido = valido & 1;
       else
         valido = valido & 0;
@@ -177,40 +197,79 @@ Nodo* ricerca_libri(Nodo* testa_lista, Libro* filtri){
       }
       
       Libro* tmp = (Libro *)malloc(sizeof(Libro));
-      copia(&tmp, testa_lista);
+      copia(tmp, cursore -> libro);
       nodo_invia -> libro = tmp;
       nodo_invia -> next = NULL;
     } 
-    testa_lista = testa_lista -> next;
+    cursore = cursore -> next;
    }
    return invia;
+}
+
+void* worker(void* arg){
+  printf("testing\n");
 }
 
 int main(void) {
   Nodo* testa_lista; //puntatore alla radice della lista
   
   testa_lista = riempi_catalogo("../data/biblioteca_1.txt");
+
+  if (pthread_mutex_init(&buffer_mutex, NULL) != 0) {
+    perror("pthread_mutex_init() error");
+    exit(1);
+  }
+
+  int num_workers = 5;
+  if (num_workers <= 0) return 0;
+  pthread_t *workers = malloc(sizeof(pthread_t) * num_workers);
+
+  // crea il buffer
+  Queue_t *buffer = initQueue();
+
+  // avvia m worker
+  for (int i = 0; i < num_workers; i++) {
+    if (pthread_create(&(workers[i]), NULL, worker, buffer) != 0) {
+      perror("pthread_create() error");
+      exit(1);
+    }
+  }
+
+  // aspetto i worker
+  for (int i = 0; i < num_workers; i++) {
+    pthread_mutex_lock(&buffer_mutex);
+    if (pthread_join(workers[i], NULL) != 0) {
+      perror("pthread_create() error");
+      exit(1);
+    }
+    pthread_mutex_unlock(&buffer_mutex);
+  }
+  free(workers);
+  pthread_mutex_destroy(&buffer_mutex);
+  deleteQueue(buffer);
+
 //  Nodo* nodo = testa_lista;
 //  while(nodo != NULL){
 //    stampa(nodo->libro);
 //    nodo = nodo -> next;
 //  }
-  Libro filtro;
-  strcpy(filtro.autore[0], "");
-  strcpy(filtro.titolo, "");
-  strcpy(filtro.editore, "");
-  filtro.anno = -1;
-  strcpy(filtro.collocazione, "");
-  strcpy(filtro.descrizione_fisica, "");
-  filtro.prestito = NULL;
 
-  Nodo* ricerca = ricerca_libri(testa_lista, &filtro);
+//  Libro filtro;
+//  strcpy(filtro.autore[0], "Daniel");
+//  strcpy(filtro.titolo, "");
+//  strcpy(filtro.editore, "");
+//  filtro.anno = -1;
+//  strcpy(filtro.collocazione, "");
+//  strcpy(filtro.descrizione_fisica, "");
+//  filtro.prestito = NULL;
 
-  Nodo* nodo = ricerca;
-  while(nodo != NULL){
-    stampa(nodo->libro);
-    nodo = nodo -> next;
-  }
+//  Nodo* ricerca = ricerca_libri(testa_lista, &filtro);
+
+//  Nodo* nodo = ricerca;
+//  while(nodo != NULL){
+//    stampa(nodo->libro);
+//    nodo = nodo -> next;
+//  }
   
   return 0;
 }
